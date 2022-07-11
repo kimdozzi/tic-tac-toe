9d17e78e9a7f06c855ff5784966407251b481980
DoHyung Kim <neo5188@gmail.com>
2022-07-11 19:21:25 +0900

    Wep APIs : FormData & Setup UserName

--- scripts/app.js
+++ scripts/app.js
@@ -1,9 +1,38 @@
+let editedPlayer = 0;
+let activePlayer = 0;
+
+const players = [
+  {
+    name: "",
+    symbol: "X",
+  },
+  {
+    name: "",
+    symbol: "O",
+  },
+];
+
 const PlayerConfigOverlayElement = document.querySelector(".config-overlay");
 const backdropElement = document.querySelector("#backdrop");
+
+const formElement = document.querySelector("form");
+const errorsOutputElement = document.querySelector("#config-errors");
 
 const editPlyaer1BtnElement = document.querySelector("#edit-player1-btn");
 const editPlyaer2BtnElement = document.querySelector("#edit-player2-btn");
 const cancelConfigBtnElement = document.querySelector(".cancel-config-btn");
+
+const startNewGameBtnElement = document.querySelector("#start-game-btn");
+const gameAreaElement = document.querySelector(".active-game");
+// const gameFieldElements = document.querySelectorAll(
+//   ".active-game__game-board li"
+// );
+const gameBoardElement = document.querySelector(".active-game__game-board");
+const activePlayerNameElement = document.querySelector(
+  ".active-game__play-name"
+);
+
+// const playerNameElement = document.querySelector("#playername");
 
 // A 파일에 있는 함수나 상수를 정의하면 B 파일에서도 사용할 수가 있다.
 // 중요한 건 정의된 순서 !
@@ -12,3 +41,12 @@ editPlyaer2BtnElement.addEventListener("click", openPlayConfig);
 
 cancelConfigBtnElement.addEventListener("click", closePlayerConfig);
 backdropElement.addEventListener("click", closePlayerConfig);
+
+formElement.addEventListener("submit", savePlayerConfig);
+
+startNewGameBtnElement.addEventListener("click", startNewGame);
+
+// for (const gameFieldElement of gameFieldElements) {
+//   gameFieldElement.addEventListener("click", selectGameField);
+// }
+gameBoardElement.addEventListener("click", selectGameField);
--- scripts/config.js
+++ scripts/config.js
@@ -1,4 +1,7 @@
-function openPlayConfig() {
+function openPlayConfig(event) {
+  const selectedPlayerId = +event.target.dataset.playerid;
+  editedPlayer = selectedPlayerId;
+
   PlayerConfigOverlayElement.style.display = "block";
   backdropElement.style.display = "block";
 }
@@ -6,4 +9,38 @@ function openPlayConfig() {
 function closePlayerConfig() {
   PlayerConfigOverlayElement.style.display = "none";
   backdropElement.style.display = "none";
+  formElement.firstElementChild.classList.remove("error");
+  errorsOutputElement.textContent = "";
+  formElement.firstElementChild.lastElementChild.value = "";
+  // playerNameElement.value = "";
+}
+
+function savePlayerConfig(event) {
+  event.preventDefault();
+  const formData = new FormData(event.target);
+  const enteredPlayername = formData.get("playername").trim(); // '    Max    ' => 'Max' trim's function
+
+  if (!enteredPlayername) {
+    // enteredPlayername === ''
+    event.target.firstElementChild.classList.add("error");
+    errorsOutputElement.textContent = "please enter a valid name!";
+    return;
+  }
+
+  const updatedPlayerDataElement = document.querySelector(
+    "#player-" + editedPlayer + "-data"
+  );
+  updatedPlayerDataElement.children[1].textContent = enteredPlayername;
+
+  // if(editedPlayer === 1) {
+  //   players[0].name = enteredPlayername;
+  // }
+  // else{
+  //   players[1].name = enteredPlayername;
+  // }
+
+  players[editedPlayer - 1].name = enteredPlayername;
+
+  closePlayerConfig();
+  errorsOutputElement.textContent = "";
 }
